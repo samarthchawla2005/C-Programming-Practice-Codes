acc8a1bce9c04a0fa0ec9342005c60c0a3f6226a
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-05-05 22:24:48 +0530

    Add files via upload

---
+++ While Loop no. from 1 to 10.c
@@ -0,0 +1,16 @@
+//Number from 1 to 10 using while loop
+
+#include<stdio.h>
+
+int main()
+{
+    int n = 1;
+
+    while(n <= 10)
+    {
+        printf("%d\n", n);
+        n++;
+    }
+    return 0;
+}
+
---
+++ While Loop.c
@@ -0,0 +1,14 @@
+//While Loop
+
+#include <stdio.h>
+
+int main()
+{
+	int i=1;
+	while(i<=5)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+}
